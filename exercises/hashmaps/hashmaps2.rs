a6d71c82abc25303dd91ddf1f77d5b1bf15322cf
tanpero <camille-dolma@outlook.com>
2025-03-21 21:03:51 +0800

    hashmap

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -37,9 +37,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
+        // 只有当水果不在篮子中时才添加
+        if !basket.contains_key(&fruit) {
+            // 添加新水果，数量为1
+            basket.insert(fruit, 1);
+        }
     }
 }
 
